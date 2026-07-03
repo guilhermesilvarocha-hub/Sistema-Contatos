79f52efd3b684e148167893970e2d5cf4b2b663d
guilhermesilvarocha-hub <guilherme.silva.rocha@aluno.edu.pi.gov.br>
2026-07-03 14:51:03 -0300

    Create cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,31 @@
+const API_BASE = "http://localhost:3000";
+
+async function carregarContatos () {
+  try {
+     const res = await fetch (`${API_BASE}/contato0s`);
+     if (!res.ok) throw new Error ("Erro ao buscar contatos");
+    const contatos = await res.json();
+    const lista = document.getElementById("listaContatos");
+    lista.innerHTML = "";
+    contatos.forEach (c=> {
+ const li = document.createElement("li");
+  li.innerHTML `
+     <strong>Nome:</strong> ${c.nome} <br>
+     <strong>telefone:</strong> ${c.telefone || "-"} <br>
+     <strong>Email:</strong> ${c.email || "-"} <br>
+     `;
+
+      lista.appendChild(li); 
+    });
+
+  } catch (err) { 
+    console.error (err);
+
+    alert ("Não foi possivel carrregar contatos.");
+  }
+}
+document.getElementById("btnCadastrar").addventListener("click", async () +> {
+   const nome = document.getElemetById("nome").value.trim();
+  const nome = document.getElemetById("nome").value.trim();
+
+       
